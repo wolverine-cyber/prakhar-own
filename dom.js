b093017f41839bb5255c61f8a1499f1144bc93b2
wolverine-cyber <prakharpandya6390@gmail.com>
2023-10-04 22:24:13 +0530

    DOMassignment5

--- dom.js
+++ dom.js
@@ -26,15 +26,56 @@
     header.style.borderBottom='solid 3px #000';
 
     //GETELEMENTSBYCLASSNAME//
-    var items = document.getElementsByClassName('list-group-item');
-    console.log(items);
-    console.log(items[1]);
-    items[1].textContext='Hello 2';
-    items[1].style.fontWeight='bold';
-    items[1].style.backgroundColor='yellow';
+    //var items = document.getElementsByClassName('list-group-item');
+    //console.log(items);
+    //console.log(items[1]);
+    //items[1].textContext='Hello 2';
+    //items[1].style.fontWeight='bold';
+   // items[1].style.backgroundColor='yellow';
 
-    for(i=0;i<items.length;i++){
-        items[i].style.backgroundColor='skyblue';
+    //for(i=0;i<items.length;i++){
+     //   items[i].style.backgroundColor='skyblue';
 
-    }
+   // }
+   //GETELEMENTSBYTAGSNAME//
+   var li = document.getElementsByTagName('li');
+   console.log(li);
+   console.log(li[1]);
+   li[1].textContent='hello 2';
+   li[1].style.fontWeight='bold';
+   li[1].style.backgroundColor='yellow';
+   for(var i=0; i<li.length;i++){
+    li[i].style.backgroundColor='red';
+
+   }
+   //QUERYSELECTOR
+/*var header= document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+var input= document.querySelector('input');
+input.value='hello world'
+var submit = document.querySelector('input[type="submit"]');
+   submit.value="SEND";
+   var item = document.querySelector('.list-group-item');
+   item.style.color='red';
+   var lastItem = document.querySelector('.list-group-item:last-child');
+   lastItem.style.color='blue';
+   var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+   lastItem.style.color='coral';
+   //QUERYSELECTORALL//
+   var titles = document.querySelectorAll('.title');
+
+  // console.log(titles);
+  // titles[0].textContent='Hello';
+
+ //  var odd = document.querySelectorAll('li:nth-child(odd)');
+ //  var odd = document.querySelectorAll('li:nth-child(even)');
+
+ //  for(var i=0; i<odd.length;i++){
+  //  odd[i].style.backgroundColor='#blue';
+ //   even[i].style.backgroundColor='#skyblue';
+
+ //  }
+   
+
+
     
